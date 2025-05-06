555c73b8368715175fe4fd5692bd557c5b33d05f
Bandile <ST10480829@vcconnect.edu.ze>
2025-05-06 09:50:27 +0200

    called components to kt folder (Flashcard Screen)

--- app/src/main/java/com/example/assignment21/FlashcardQuestionScreen.kt
+++ app/src/main/java/com/example/assignment21/FlashcardQuestionScreen.kt
@@ -1,6 +1,8 @@
 package com.example.assignment21
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,14 @@ class FlashcardQuestionScreen : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.flashcard_question_screen)
 
+        //==========================================================================================
+        //calling components from xml folder to kt folder.
+        val txtContext = findViewById<TextView>(R.id.txt_Context)
+        val txtHistQuiz = findViewById<TextView>(R.id.txt_HistQuiz)
+        val btnTrue = findViewById<Button>(R.id.btn_True)
+        val btnFalse = findViewById<Button>(R.id.btn_False)
+        val btnContinue = findViewById<Button>(R.id.btn_Continue)
+        val txtQuestion = findViewById<TextView>(R.id.txt_Question)
+        //==========================================================================================
     }
 }
