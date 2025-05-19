3db6859d91a4b0719218b75ea8877979b51bf5a5
Bandile <ST10480829@vcconnect.edu.ze>
2025-05-19 02:57:59 +0200

    FeedBack WORKS!

--- app/src/main/java/com/example/assignment21/FlashcardQuestionScreen.kt
+++ app/src/main/java/com/example/assignment21/FlashcardQuestionScreen.kt
@@ -10,50 +10,65 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FlashcardQuestionScreen : AppCompatActivity() {
+    //==============================================================================================
+    // declaring components as private variables.
+    private var score = 0
+    private var questionIndex = 0
+    private var select = false
+    private var ans = false
+    private lateinit var btnTrue: Button
+    private lateinit var btnFalse: Button
+    private lateinit var btnNext:  Button
+    private lateinit var txtContext :TextView
+    private lateinit var txtHistQuiz:TextView
+    private lateinit var txtFeedback:TextView
+    private lateinit var txtUserAns :TextView
+    private lateinit var txtUserInfo:TextView
+    //==============================================================================================
+
+    //==============================================================================================
+    // declaring private array.
+    private val arrQuiz = arrayOf("Q1", "Q2", "Q3", "Q4", "Q5")
+    private val arrAns = booleanArrayOf(true, false, true, false, true)
+    //private val arrUserAns = booleanArrayOf()
+    //==============================================================================================
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.flashcard_question_screen)
+
 
         //==========================================================================================
         //Calling components from xml folder to kt folder.
-        val txtContext = findViewById<TextView>(R.id.txt_Context)
-        val txtHistQuiz = findViewById<TextView>(R.id.txt_HistQuiz)
-        val btnTrue = findViewById<Button>(R.id.btn_True)
-        val btnFalse = findViewById<Button>(R.id.btn_False)
-        val btnNext = findViewById<Button>(R.id.btn_Next)
-        val txtFeedback =findViewById<TextView>(R.id.txt_FeedBack)
-        val txtUserAns =findViewById<TextView>(R.id.txt_UserAns)
-        val txtUserInfo =findViewById<TextView>(R.id.txt_UserInfo)
-
-        //==========================================================================================
-
-        //==========================================================================================
-        // Declaring and populating arrays.
-        val arrQuiz = arrayOf("Q1", "Q2", "Q3", "Q4", "Q5")
-        val arrAns = arrayOf("true", "false", "true", "false", "true")
+        btnTrue = findViewById(R.id.btn_True)
+        btnFalse = findViewById(R.id.btn_False)
+        btnNext = findViewById(R.id.btn_Next)
+        txtContext = findViewById(R.id.txt_Context)
+        txtHistQuiz = findViewById(R.id.txt_HistQuiz)
+        txtFeedback = findViewById(R.id.txt_FeedBack)
+        txtUserAns =findViewById(R.id.txt_UserAns)
+        txtUserInfo =findViewById(R.id.txt_UserInfo)
         //==========================================================================================
 
         //==========================================================================================
         //True and false button assigning value.
-        btnTrue.setOnClickListener {txtUserAns.text= "true" }
-        btnFalse.setOnClickListener {txtUserAns.text="false" }
+
+        btnTrue.setOnClickListener {ans=true }
+        btnFalse.setOnClickListener {ans=false }
         //==========================================================================================
         var index = 0
-        val userAns=txtUserAns.text.toString()
-        txtHistQuiz.text=arrQuiz[index]
+        val userAns = txtUserAns.text.toString()
+        txtHistQuiz.text = arrQuiz[index]
 
         btnNext.setOnClickListener{
             if (index >= 4){
                 val flashcardToScore = Intent(this,ScoreScreen ::class.java)
-                startActivity(flashcardToScore)
-            }
-            else{
-                txtHistQuiz.text=arrQuiz[index+1]
-          if (userAns==arrAns[index]) {txtFeedback.text="Correct"}
-          else{txtFeedback.text="Incorrect"}
-                index++
-        }}
+                startActivity(flashcardToScore) }
+            else{   txtHistQuiz.text=arrQuiz[index+1]
+                if(arrAns[index]==ans){ txtFeedback.text = "Correct"}
+                else{txtFeedback.text = "incorrect"}
+                //arrUserAns[index]= ans
+                index++}
+        }
 
 
 
--- app/src/main/java/com/example/assignment21/ScoreScreen.kt
+++ app/src/main/java/com/example/assignment21/ScoreScreen.kt
@@ -1,18 +1,22 @@
 package com.example.assignment21
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ScoreScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.score_screen)
+
+
+
        //===========================================================================================
         //Call all components from xml folder to kt folder.
         val btnExit = findViewById<Button>(R.id.btn_Exit)
@@ -21,6 +25,12 @@ class ScoreScreen : AppCompatActivity() {
         val txtScore = findViewById<TextView>(R.id.txt_Score)
         //==========================================================================================
 
+
         btnExit.setOnClickListener{finishAffinity()}
+        btnReview.setOnClickListener{
+
+
+        }
     }
+
 }
